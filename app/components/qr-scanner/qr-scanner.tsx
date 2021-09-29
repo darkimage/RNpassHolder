e4f886f5b89606b67d05f57512a6d300dfa1093f
Darkimage <luc-af@live.it>
2021-09-29 10:15:16 +0200

    Added overlay to QRScanner Refactored style for QRScanner

--- app/components/qr-scanner/qr-scanner.tsx
+++ app/components/qr-scanner/qr-scanner.tsx
@@ -2,20 +2,27 @@ import * as React from "react"
 import { StyleProp, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
 import QRCodeScanner, { RNQRCodeScannerProps } from "react-native-qrcode-scanner"
-import { Modal, StyleService, Text, useStyleSheet, Button, Card } from "@ui-kitten/components"
+import { Modal, StyleService, Text, useStyleSheet, Button, Card, Layout, Divider } from "@ui-kitten/components"
 import { translate } from "../../i18n"
+import SVGScannerIcon from '../../../assets/svg/qr-scanner-icon.svg'
 
 export interface QrScannerProps extends RNQRCodeScannerProps {
   /**
    * An optional style override useful for padding & margin.
    */
   style?: StyleProp<ViewStyle>,
+  /**
+   * Modal State
+   */
   show: boolean,
+  /**
+   * User as exited the modal view
+   */
   onCancel: () => void
 }
 
 /**
- * Describe your component here
+ * A Modal QR Scanner interface
  */
 export const QrScanner = observer(function QrScanner(props: QrScannerProps) {
   const { style, show, onCancel } = props
@@ -25,26 +32,65 @@ export const QrScanner = observer(function QrScanner(props: QrScannerProps) {
     <Modal
       backdropStyle={styles.BACKDROP}
       visible={show}>
-      <View style={[styles.ROOT, style]}>
+      <Card
+        style={styles.CARD}
+        header={() => <View style={styles.CENTER}>
+          <Text category="h4" style={styles.TITLE}>{translate('qrCodeScanner.title')}</Text>
+        </View>}
+        footer={<View style={styles.CENTER}>
+          <Button onPress={onCancel} appearance="ghost">
+            {translate('common.cancel')}
+          </Button>
+        </View>}
+      >
         <View style={styles.QRSCANNER}>
+          <View style={styles.OVERLAY}>
+            <SVGScannerIcon width={200} height={200}  />
+          </View>
           <QRCodeScanner
             onRead={(e) => props.onRead(e)}
             containerStyle={styles.CAMERACONTAINER}
             cameraStyle={styles.CAMERA}
-            topContent={<Text category="h3">{translate('qrCodeScanner.title')}</Text> }
           />
         </View>
-        <Button onPress={onCancel} style={styles.CANCEL}>{translate('common.cancel')}</Button>
-      </View>
+      </Card>
     </Modal>
   )
 })
 
 
 const styleComp = StyleService.create({
+  CARD: {
+    flex: 1,
+    margin: 2,
+    borderRadius: 20,
+    elevation: 12
+  },
+  CENTER: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   QRSCANNER: {
     overflow: 'hidden',
     borderRadius: 20,
+    maxWidth: 250,
+    minHeight: 250,
+    flex: 1,
+    position: 'relative',
+    alignItems: "center"
+  },
+  TITLE: {
+    padding: 16,
+  },
+  OVERLAY: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0, bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 99
   },
   CAMERACONTAINER: {
     alignItems: 'center',
@@ -57,8 +103,9 @@ const styleComp = StyleService.create({
     overflow: 'hidden',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
     flex: 1,
-    padding: 32,
+    paddingVertical: 32,
     width: 300
   },
   BACKDROP: {
